15d68bfb02f85a7755827520afd8fb470f1640e1
klownjf1 <mastervinaka@gmail.com>
2022-12-15 12:31:50 +0200

    commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let select = document.querySelector('.select')
+let img = document.querySelector('.img')
+
+img.src = select.value
+
+
+function handler() {
+    let getValue = select.value;    
+    img.src = getValue
+}
+
+function width() {
+    let img = document.querySelector('.img')    
+    let pr = prompt('Введите ширину')    
+    img.style.width = pr + 'px'    
+}
+
+function height() {
+    let img = document.querySelector('.img')     
+    let pr = prompt('Введите высоту')    
+    img.style.height = pr + 'px'    
+}
+
+function border() {
+    let img = document.querySelector('.img')        
+    let pr = prompt('Введите ширину рамки')   
+    img.style.border = pr + 'px solid black'    
+}
+
+function alt() {
+    let img = document.querySelector('.img')        
+    let pr = prompt('Введите альтернативный текст')   
+    img.alt = pr    
+}
+
