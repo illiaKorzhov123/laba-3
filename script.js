ee53b50f486774a9902e8c7018517f3064f2d50f
Illia <illia.korzhov.ua@gmail.com>
2023-05-21 13:54:17 +0000

    fix alignment

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ img.src = select.value
 
 
 function handler() {
-    let getValue = select.value;    
-    img.src = getValue
+	let getValue = select.value
+	img.src = getValue
 }
 
 function width() {
-    let img = document.querySelector('.img')    
-    let pr = prompt('Введите ширину')    
-    img.style.width = pr + 'px'    
+	let img = document.querySelector('.img')
+	let pr = prompt('Введите ширину')
+	img.style.width = pr + 'px'
 }
 
 function height() {
-    let img = document.querySelector('.img')     
-    let pr = prompt('Введите высоту')    
-    img.style.height = pr + 'px'    
+	let img = document.querySelector('.img')
+	let pr = prompt('Введите высоту')
+	img.style.height = pr + 'px'
 }
 
 function border() {
-    let img = document.querySelector('.img')        
-    let pr = prompt('Введите ширину рамки')   
-    img.style.border = pr + 'px solid black'    
+	let img = document.querySelector('.img')
+	let pr = prompt('Введите ширину рамки')
+	img.style.border = pr + 'px solid black'
 }
 
 function alt() {
-    let img = document.querySelector('.img')        
-    let pr = prompt('Введите альтернативный текст')   
-    img.alt = pr    
+	let img = document.querySelector('.img')
+	let pr = prompt('Введите альтернативный текст')
+	img.alt = pr
 }
 
